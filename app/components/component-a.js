83661613a4dfbad51452fd9b52f451886472b623
Kyle Hornberg <kyle.hornberg@gmail.com>
2015-10-12 16:31:54 -0500

    Fix component a

--- app/components/component-a.js
+++ app/components/component-a.js
@@ -7,7 +7,7 @@ export default Ember.Component.extend({
     click: function(sent_event_from_hbs) {
       // get the data from the elements of the component
       let o = {};
-      this.get("childViews").forEach((v) => { o[v.name] = v.value; } )
+      this.get("childViews").forEach((v) => { o[v.name] = v.value; } );
       this.set('formData', o);
       // formData is passed through the event bus to component b
       this.get('eventbus').trigger(sent_event_from_hbs, this.get('formData'));
